08127dfe12b0053ec13c346b131f395f685c7e87
Serge <oliynyk.serge@gmail.com>
2018-10-18 20:30:28 -0500

    The project is unlinked from Git

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.sandbox;
 
 public class MyFirstProgram {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 
 }
