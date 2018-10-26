913d8667fe28f936ddad464d0533ec4bcb25441b
Serge <oliynyk.serge@gmail.com>
2018-10-26 15:49:19 -0500

    Several methods were added

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -2,7 +2,28 @@ package ru.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("World");
+        hello("User");
+        hello("Serge");
+
+        double len = 6;
+        System.out.println("Area with side " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("Area with sides " + a + " and " + b + " = " + area(a, b));
+    }
+
+    public static void hello (String who) {
+                System.out.println("Hello, " + who + "!");
+    }
+
+    public static double area (double l) {
+        return l * l;
+    }
+
+    public static double area (double a, double b){
+        return a * b;
     }
 
 }
