faaad2e2e3c7b997230044bd59b76b6d49060a56
Serge <oliynyk.serge@gmail.com>
2018-10-30 23:30:55 -0500

    Update addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.addressbook.appmanager;
 
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -13,15 +12,6 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-
-  public static boolean isAlertPresent(FirefoxDriver wd){
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public void init() {
     System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver.exe");
