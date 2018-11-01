86db590c6e1d39dfafc06516d32069b87f9b3a32
Serge <oliynyk.serge@gmail.com>
2018-11-01 10:48:55 -0500

    HelperBase class updated

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/HelperBase.java
@@ -23,6 +23,10 @@ public class HelperBase {
     wd.findElement(locator).sendKeys(text);
   }
 
+  protected void closeAlertWindow () {
+    wd.switchTo().alert().accept();
+  }
+
   public boolean isAlertPresent(){
     try {
       wd.switchTo().alert();
