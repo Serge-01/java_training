e8e0a71341c6d449f3108bbd44579b9e26cd6340
Serge <oliynyk.serge@gmail.com>
2018-12-07 23:22:55 -0600

    Aded Prime Class and Tests

--- sandbox/src/main/java/ru/sandbox/Primes.java
+++ sandbox/src/main/java/ru/sandbox/Primes.java
@@ -10,4 +10,53 @@ public class Primes {
     }
     return true;
   }
+
+  public static boolean isPrimeWhile(int n){
+    int i = 2;
+    while (i < n){
+      if(n % i == 0){
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+
+  public static boolean isPrimeWhile2(int n){
+    int i = 2;
+    while (i < n && n % i != 0){
+      i++;
+    }
+    return i == n;
+  }
+
+  public static boolean isPrime(long n){
+    for(long i = 2; i < n; i++){
+      if(n % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast(int n){
+    for(int i = 2; i < n/2; i++){
+      if(n % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast2(int n){
+    int m = (int) Math.sqrt(n);
+    for(int i = 2; i < m; i++){
+      if(n % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+
 }
