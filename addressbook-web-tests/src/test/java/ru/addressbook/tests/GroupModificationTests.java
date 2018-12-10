c779ed3d64cdf6215e1536f7f7a0866f9e69cd61
Serge <oliynyk.serge@gmail.com>
2018-12-09 21:35:03 -0600

    Updated Group Tests

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test2", "testing group", "testing group"));
     }
@@ -16,6 +18,8 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("EditTest", "Modifier1", "Modifier1"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
   }
 
 }
